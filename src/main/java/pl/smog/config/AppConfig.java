1530f679f69afbd98939fb76fa38b517a4f22185
KarolNurzynski <karolnurzynski@gmail.com>
2019-03-22 03:36:09 +0100

    changes config to connect to db

--- src/main/java/pl/smog/config/AppConfig.java
+++ src/main/java/pl/smog/config/AppConfig.java
@@ -3,6 +3,12 @@ package pl.smog.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -16,6 +22,20 @@ public class AppConfig {
     @Bean
     ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDbFactory mongoDbFactory,
+                                       MongoMappingContext context) {
+
+        MappingMongoConverter converter =
+                new MappingMongoConverter(new DefaultDbRefResolver(mongoDbFactory), context);
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory, converter);
+
+        return mongoTemplate;
+
     }
 
 }
